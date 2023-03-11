cbbd8bab41e17b587c1fc641f0ee76eba2ecdda2
Birdick <birdick@mail.ru>
2023-03-11 22:42:08 +0300

    Cont

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+string[] GetArray;
+Console.WriteLine("Enter any digits:");
+string row = Console.ReadLine()!;
+GetArray = row.Split(' ');
+var Sum = new string[GetArray.Length];
+var IntSum = 0;
+foreach (var value in GetArray)
+{
+    if (value.Length <= 3)
+    {
+        Sum[IntSum] = value;
+        IntSum++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, Sum, 0, IntSum));
+Console.WriteLine();
---
+++ Readme.md
@@ -0,0 +1,9 @@
+In order to do the work we have to start with:
+* Create new repository on GitHub;
+* Connect the created repository with our work on personal PC.
+
+Then we have to work out the algorithm block-scheme.
+
+Having this step done we can create the programm and check it works properly.
+
+The last step to make is place the work in our repository on GitHub
